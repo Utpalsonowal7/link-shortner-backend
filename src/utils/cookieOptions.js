6590971c5a9cf2dbdf023f0cace8611df6314b05
Utpal Sonowal <sonowalu73@gmail.com>
2026-07-01 19:21:23 +0530

    same site problem solve

--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -1,12 +1,14 @@
 const accessTokenOptions = {
      httpOnly: true,
      secure: true,
+     sameSite: "none",
      maxAge: 15 * 60 * 1000
 };
 
 const refreshTokenOptions = {
      httpOnly: true,
      secure: true,
+     sameSite: "none",
      maxAge: 30 * 24 * 60 * 60 * 1000
 };
 
