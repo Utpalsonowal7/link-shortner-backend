30e30874e42195e4cd13e537bc64c57ba487ef1a
Utpal Sonowal <sonowalu73@gmail.com>
2026-07-01 22:11:54 +0530

    fffvvv

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import { clientDetails } from "./middlewares/metaData.middleware.js";
 
 const app = express();
 
-app.set("trust proxy", 1);
+app.set("trust proxy", true);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(cookieParser());
--- src/middlewares/metaData.middleware.js
+++ src/middlewares/metaData.middleware.js
@@ -3,9 +3,7 @@ import { getClientInfo } from "../utils/parseDevice.js";
 export const clientDetails = (req, res, next) => {
      const userAgent = req.get("user-agent") || "";
      const data = getClientInfo(userAgent);
-console.log("req.ip:", req.ip);
-console.log("x-forwarded-for:", req.headers["x-forwarded-for"]);
-console.log("user-agent:", req.get("User-Agent"));
+
      req.clientInfo = {
           ipAddress: req.ip,
           device: data.device,
