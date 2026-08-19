60dd30563642ab54ac8dc782b8f12a851188ff50
Utpal Sonowal <sonowalu73@gmail.com>
2026-08-19 13:50:27 +0530

    fixed

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,8 +2,11 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 
-const uploadDir = path.join(process.cwd(), "public", "images");
-
+const uploadDir =
+     process.env.NODE_ENV === "production"
+          ? "/tmp"
+          : path.join(process.cwd(), "public", "images");
+          
 if (!fs.existsSync(uploadDir)) {
      fs.mkdirSync(uploadDir, { recursive: true });
 }
