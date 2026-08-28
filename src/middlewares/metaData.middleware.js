5188f1d4f7045c9686c7d5741e18afdd6f2f8554
Utpal Sonowal <sonowalu73@gmail.com>
2026-08-28 21:34:06 +0530

    ip config problem

--- src/middlewares/metaData.middleware.js
+++ src/middlewares/metaData.middleware.js
@@ -1,19 +1,13 @@
 import { getClientInfo } from "../utils/parseDevice.js";
 
 export const clientDetails = (req, res, next) => {
-     console.log({
-          ip: req.ip,
-          ips: req.ips,
-          forwarded: req.get("x-forwarded-for"),
-          realIp: req.get("x-real-ip"),
-          cfIp: req.get("cf-connecting-ip"),
-     });
+
 
      const userAgent = req.get("user-agent") || "";
      const data = getClientInfo(userAgent);
 
      req.clientInfo = {
-          ipAddress: req.ip,
+          ipAddress: req.get("cf-connecting-ip") || req.ip,
           device: data.device,
           browser: data.browser,
           os: data.os,
