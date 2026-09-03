240134b7b0504f02a70be74dd5419178d16d4dc7
Utpal Sonowal <sonowalu73@gmail.com>
2026-09-03 22:08:44 +0530

    testing

--- src/middlewares/metaData.middleware.js
+++ src/middlewares/metaData.middleware.js
@@ -4,12 +4,12 @@ export const clientDetails = (req, res, next) => {
      const userAgent = req.get("user-agent") || "";
      const data = getClientInfo(userAgent);
 
-     console.log("req.ip:", req.ip);
+    
      console.log("x-forwarded-for:", req.headers["x-forwarded-for"]);
      console.log("x-real-ip:", req.headers["x-real-ip"]);
      console.log("cf-connecting-ip:", req.headers["cf-connecting-ip"]);
      console.log("socket remoteAddress:", req.socket?.remoteAddress);
-     console.log("All headers:", req.headers);
+   
 
      req.clientInfo = {
           ipAddress: req.get("cf-connecting-ip") || req.ip,
